6202a910e11046d511f8694150a35919b5e0bfed
lzz <599218337@qq.com>
2024-12-30 15:01:59 +0800

    feat: 修改样式

--- src/components/WDPMap/class/AddCamera.js
+++ src/components/WDPMap/class/AddCamera.js
@@ -6,33 +6,33 @@
  * @FilePath: \zwyl-project-51world\screen-web\src\components\WDPMap\class\CameraTrace.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import utils from "@/utils/utils.js";
+import utils from "@/utils/utils.ts";
 
 
 class AddCamera {
 
-    constructor(options) {
-        this.cloudRender = options.cloudRender;
-        this.name="AddCamera添加相机镜头"
+  constructor(options) {
+    this.cloudRender = options.cloudRender;
+    this.name = "AddCamera添加相机镜头"
 
-        utils.bindAll([
-            'init',
-        ], this);
-        this.init();
-    }
+    utils.bindAll([
+      'init',
+    ], this);
+    this.init();
+  }
 
-    init() {
-        console.log('AddCamera-init');
-    }
-    add() { 
-    }
-    destroy() {
-        
-    }
-    
+  init() {
+    console.log('AddCamera-init');
+  }
+  add() {
+  }
+  destroy() {
 
-    
-        
+  }
+
+
+
+
 };
-        
+
 export default AddCamera;
--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -7,24 +7,24 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
-    <div class="header-box">
-        <div class="time-box">
-            <div>
-                <span>{{ week }}</span>&nbsp&nbsp
-                <span>{{ weather }}</span>
-            </div>
-            <div>
-                <span>{{ time }}</span>
-            </div>
-        </div>
+  <div class="header-box">
+    <div class="time-box">
+      <div>
+        <span>{{ week }}</span>&nbsp&nbsp
+        <span>{{ weather }}</span>
+      </div>
+      <div>
+        <span>{{ time }}</span>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup>
 import { ref, watch, nextTick, onUnmounted } from 'vue'
 import { useStore } from "vuex";
 import axios from 'axios'
-import utils from '@/utils/utils.js'
+import utils from '@/utils/utils'
 
 import { decryptData } from "@/utils/crypto";
 const store = useStore();
@@ -35,44 +35,44 @@ let time = ref('');
 let timer = null, weatherTimer = null
 
 nextTick(() => {
-    getWeather()
-    timer = setInterval(() => {
-        week.value = utils.dateToWeek(new Date());
-        time.value = utils.dateFormat(new Date(), 'yyyy年MM月dd日');
-    }, 1000);
+  getWeather()
+  timer = setInterval(() => {
+    week.value = utils.dateToWeek(new Date());
+    time.value = utils.dateFormat(new Date(), 'yyyy年MM月dd日');
+  }, 1000);
 
-    weatherTimer = setInterval(() => {
-        getWeather()
-    }, 600000);
-    // const user_s = localStorage.getItem('account_name');
-    // userName.value = decryptData(user_s);
+  weatherTimer = setInterval(() => {
+    getWeather()
+  }, 600000);
+  // const user_s = localStorage.getItem('account_name');
+  // userName.value = decryptData(user_s);
 });
 onUnmounted(() => {
-    clearInterval(timer)
-    clearInterval(weatherTimer)
+  clearInterval(timer)
+  clearInterval(weatherTimer)
 })
 
 
 const getWeather = () => {
-    axios.get('https://restapi.amap.com/v3/weather/weatherInfo', {
-        params: {
-            key: 'c4d5a8f8b9d2f351d0bd7ec75c447707',
-            city: '420505',
-            // extensions:'base'
-        },
-    }).then(
-        (res) => {
-            if (res.data.count == '1') {
-                weather.value = res.data.lives[0].weather
-                // store.dispatch('setWeather',weather.value)
-            } else {
-                weather.value = '未知'
-            }
-        }
-    )
+  axios.get('https://restapi.amap.com/v3/weather/weatherInfo', {
+    params: {
+      key: 'c4d5a8f8b9d2f351d0bd7ec75c447707',
+      city: '420505',
+      // extensions:'base'
+    },
+  }).then(
+    (res) => {
+      if (res.data.count == '1') {
+        weather.value = res.data.lives[0].weather
+        // store.dispatch('setWeather',weather.value)
+      } else {
+        weather.value = '未知'
+      }
+    }
+  )
 }
 
-const formatWeather=()=>{
+const formatWeather = () => {
 
 }
 
